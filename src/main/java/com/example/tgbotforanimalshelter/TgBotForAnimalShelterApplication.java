738463bd347743c972ae57eae72bd2c3fe764082
masha <mativaclub@gmail.com>
2023-10-02 11:46:39 +0300

    Initial commit

---
+++ src/main/java/com/example/tgbotforanimalshelter/TgBotForAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package com.example.tgbotforanimalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotForAnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgBotForAnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tgbotforanimalshelter/TgBotForAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgbotforanimalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotForAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
